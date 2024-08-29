a907811a628cc6d56f322eed2bea6ab6c8c93aec
Sergey Angelov <mrangel@yandex.ru>
2024-08-29 17:53:41 +0300

    Готовая задача

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -4,11 +4,18 @@ import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class AppConfig {
- 
-    @Bean(name="helloworld")
+
+    @Bean(name = "helloworld")
     public HelloWorld getHelloWorld() {
         HelloWorld helloWorld = new HelloWorld();
         helloWorld.setMessage("Hello World!");
         return helloWorld;
+    }
+
+    @Scope(scopeName = "prototype")
+    @Bean(name = "cat")
+    public Cat getHello() {
+        Cat cat = new Cat();
+        return cat;
     }
 }
